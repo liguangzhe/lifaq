b74ef7dc99e1f62da9c073ca73210770bec8a10b
lgz0012 <gl279@njit.edu>
2018-11-21 03:02:03 -0500

    added a list of questions on homepage and a pager.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Questions
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+
+                        <div class="card-body">
+
+                            <div class="card-deck">
+                                @foreach($questions as $question)
+                                    <div class="col-sm-4 d-flex align-items-stretch">
+                                        <div class="card mb-3 ">
+                                            <div class="card-header">
+                                                <small class="text-muted">
+                                                    Updated: {{ $question->created_at->diffForHumans() }}
+
+                                                </small>
+                                            </div>
+                                            <div class="card-body">
+                                                <p class="card-text">{{$question->body}}</p>
+                                            </div>
+                                            <div class="card-footer">
+                                                <p class="card-text">
+
+                                                    <a class="btn btn-primary float-right" href="#">
+                                                        View
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                @endforeach
+
+
+                            </div>
+
                         </div>
-                    @endif
+                        <div class="card-footer">
+                            <div class="float-right">
+                                {{ $questions->links() }}
+                            </div>
+                        </div>
 
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection
