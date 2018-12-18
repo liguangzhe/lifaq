d55c701795ea905384429dd67f20c1410cb5d032
lgz0012 <gl279@njit.edu>
2018-12-18 02:43:45 -0500

    I added the comment view route for my page

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,23 @@ Route::get('/user/{user_id}/profile/{profile_id}/edit', 'ProfileController@edit'
 Route::post('/user/{user_id}/profile/', 'ProfileController@store')->name('profile.store');
 Route::patch('/user/{user_id}/profile/{profile_id}', 'ProfileController@update')->name('profile.update');
 Route::delete('/user/{user_id}/profile/{profile_id}', 'ProfileController@destroy')->name('profile.destroy');
+
 Route::get('/questions/{question_id}/answers/create', 'AnswerController@create')->name('answers.create');
 Route::get('/questions/{question_id}/answers/{answer_id}', 'AnswerController@show')->name('answers.show');
 Route::get('/questions/{question_id}/answers/{answer_id}/edit', 'AnswerController@edit')->name('answers.edit');
 Route::post('/questions/{question_id}/answers/', 'AnswerController@store')->name('answers.store');
 Route::patch('/questions/{question_id}/answer/{answer_id}', 'AnswerController@update')->name('answers.update');
 Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@destroy')->name('answers.destroy');
+
+Route::get('/answers/{answer_id}/comments/create', 'CommentController@create')->name('comments.create');
+Route::get('/answers/{answer_id}/comments/{comment_id}', 'CommentController@show')->name('comments.show');
+Route::get('/answers/{answer_id}/comments/{comment_id}/edit', 'CommentController@edit')->name('comments.edit');
+Route::post('/answers/{answer_id}/comments/', 'CommentController@store')->name('comments.store');
+Route::patch('/answers/{answer_id}comment/{comment_id}', 'CommentController@update')->name('comments.update');
+Route::delete('/answers/{answer_id}/comment/{comment_id}', 'CommentController@destroy')->name('comments.destroy');
+
+
+
 Route::resources([
     'questions' => 'QuestionController',
 ]);
