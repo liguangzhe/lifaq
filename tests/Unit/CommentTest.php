f69e62a382ad9b2a9f310f34c276ad04e4ebee84
lgz0012 <gl279@njit.edu>
2018-12-19 19:16:54 -0500

    I created a comment save test and it works

---
+++ tests/Unit/CommentTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CommentTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSave()
+    {
+        $user = factory(\App\User::class)->make();
+        $user->save();
+        $question = factory(\App\Question::class)->make();
+        $question->user()->associate($user);
+        $question->save();
+        $answer = factory(\App\Answer::class)->make();
+        $answer->user()->associate($user);
+        $answer->question()->associate($question);
+        $answer->save();
+        $comment = factory(\App\Comment::class)->make();
+        $comment->user()->associate($user);
+        $comment->answer()->associate($answer);
+
+        $this->assertTrue($comment->save());
+    }
+
+
+}
